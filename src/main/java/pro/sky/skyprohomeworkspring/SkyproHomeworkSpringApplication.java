218dfde76c1b43b68c991a135059d0d23c2b18e1
grigoriirarog <rarog.grigoriii@gmail.com>
2023-06-08 21:18:47 +0200

    Initial commit

---
+++ src/main/java/pro/sky/skyprohomeworkspring/SkyproHomeworkSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohomeworkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomeworkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproHomeworkSpringApplication.class, args);
+	}
+
+}
